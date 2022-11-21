f35fa5138af848344331e40d2f553cd8de8f01d9
Giovanni Berti <giovanni.berti@pagopa.it>
2022-11-21 12:24:57 +0100

    docs: add missing documentation

--- src/main/java/it/pagopa/ecommerce/commons/domain/PaymentToken.java
+++ src/main/java/it/pagopa/ecommerce/commons/domain/PaymentToken.java
@@ -5,7 +5,7 @@ import it.pagopa.ecommerce.commons.annotations.ValueObject;
 /**
  * <p>A payment token. This allows a client to execute a payment in the pagoPA platform.</p>
  * <p>Note that a single token is subject to expiry and may be used multiple times according to the Nodo API specifications.</p>
- * @param value
+ * @param value raw value of the payment token
  */
 @ValueObject
 public record PaymentToken(String value) {}
