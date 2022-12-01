e5db8f265744a7b8b4d7d0c4dc99745047c732f1
Fernando-Granato <fernando.granato@pagopa.it>
2022-12-01 10:42:45 +0100

    Feat : added class with componentScan

---
+++ src/main/java/it/pagopa/ecommerce/commons/ConfigScan.java
@@ -0,0 +1,11 @@
+package it.pagopa.ecommerce.commons;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <p>The class used for create a configuration scan into the project</p>
+ */
+@ComponentScan
+@Configuration
+public class ConfigScan {
+}
