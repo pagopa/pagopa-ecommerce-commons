7758cf0be5f455eea748553193649b87861bacf0
Gianluca Ciuffa <gianlucaciuffa@MBP-di-Gianluca.homenet.telecomitalia.it>
2023-02-01 16:46:54 +0100

    remove unused import

--- src/main/java/it/pagopa/ecommerce/commons/documents/TransactionAddReceiptData.java
+++ src/main/java/it/pagopa/ecommerce/commons/documents/TransactionAddReceiptData.java
@@ -1,7 +1,6 @@
 package it.pagopa.ecommerce.commons.documents;
 
 import it.pagopa.ecommerce.commons.generated.server.model.TransactionStatusDto;
-import it.pagopa.generated.ecommerce.nodo.v2.dto.ClosePaymentResponseDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
