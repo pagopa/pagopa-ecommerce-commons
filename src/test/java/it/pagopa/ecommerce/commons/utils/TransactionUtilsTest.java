ab248ebddc98ff6f3895685a12af4c434a4209fe
Giovanni Berti <giovanni.berti@pagopa.it>
2023-02-13 17:59:20 +0100

    feat(utils): add utility function to get transaction fee

--- src/test/java/it/pagopa/ecommerce/commons/utils/TransactionUtilsTest.java
+++ src/test/java/it/pagopa/ecommerce/commons/utils/TransactionUtilsTest.java
@@ -1,12 +1,20 @@
 package it.pagopa.ecommerce.commons.utils;
 
+import it.pagopa.ecommerce.commons.TransactionTestUtils;
+import it.pagopa.ecommerce.commons.documents.TransactionAuthorizationRequestedEvent;
+import it.pagopa.ecommerce.commons.domain.TransactionActivated;
+import it.pagopa.ecommerce.commons.domain.TransactionWithRequestedAuthorization;
 import it.pagopa.ecommerce.commons.generated.server.model.TransactionStatusDto;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.ZonedDateTime;
 import java.util.List;
+import java.util.Optional;
 
+import static it.pagopa.ecommerce.commons.utils.TransactionUtils.getTransactionFee;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(MockitoExtension.class)
@@ -54,5 +62,29 @@ public class TransactionUtilsTest {
                     );
                 }
         );
+    }
+
+    @Test
+    void getFeeReturnsTransactionFeeForRequestedAuthorizationTransaction() {
+        TransactionAuthorizationRequestedEvent authorizationRequestedEvent = TransactionTestUtils
+                .transactionAuthorizationRequestedEvent();
+        TransactionActivated transactionActivated = TransactionTestUtils
+                .transactionActivated(ZonedDateTime.now().toString());
+        TransactionWithRequestedAuthorization tx = TransactionTestUtils
+                .transactionWithRequestedAuthorization(authorizationRequestedEvent, transactionActivated);
+
+        int expected = tx.getTransactionAuthorizationRequestData().getFee();
+
+        assertEquals(getTransactionFee(tx), Optional.of(expected));
+    }
+
+    @Test
+    void getFeeReturnsEmptyForTransactionWithoutRequestedAuthorization() {
+        TransactionAuthorizationRequestedEvent authorizationRequestedEvent = TransactionTestUtils
+                .transactionAuthorizationRequestedEvent();
+        TransactionActivated transactionActivated = TransactionTestUtils
+                .transactionActivated(ZonedDateTime.now().toString());
+
+        assertEquals(getTransactionFee(transactionActivated), Optional.empty());
     }
 }
