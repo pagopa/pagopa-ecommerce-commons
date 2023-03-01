5ebcb469b99080d12ddd1f34a4d203f7aa018f8b
Pietro Tota <pietro.tota@pagopa.it>
2023-03-01 16:57:24 +0100

    fix: remove illegal state transition from expired

--- src/main/java/it/pagopa/ecommerce/commons/domain/v1/TransactionExpired.java
+++ src/main/java/it/pagopa/ecommerce/commons/domain/v1/TransactionExpired.java
@@ -1,11 +1,9 @@
 package it.pagopa.ecommerce.commons.domain.v1;
 
 import it.pagopa.ecommerce.commons.documents.v1.TransactionExpiredEvent;
-import it.pagopa.ecommerce.commons.documents.v1.TransactionRefundErrorEvent;
 import it.pagopa.ecommerce.commons.documents.v1.TransactionRefundRequestedEvent;
 import it.pagopa.ecommerce.commons.domain.v1.pojos.BaseTransaction;
 import it.pagopa.ecommerce.commons.domain.v1.pojos.BaseTransactionExpired;
-import it.pagopa.ecommerce.commons.domain.v1.pojos.BaseTransactionWithRefundRequested;
 import it.pagopa.ecommerce.commons.domain.v1.pojos.BaseTransactionWithRequestedAuthorization;
 import it.pagopa.ecommerce.commons.generated.server.model.TransactionStatusDto;
 import lombok.EqualsAndHashCode;
@@ -43,20 +41,20 @@ public final class TransactionExpired extends BaseTransactionExpired implements 
     public Transaction applyEvent(Object event) {
         /*
          * All statuses from which a transaction will come from have the authorization
-         * been requested. The only status that has an exception from this is an expiration on ACTIVATED
-         * status that will make the transaction goes to the EXPIRED_NOT_AUTHORIZED final state.
-         * So is safe to assume that this applyEvent method will only be called for a transaction
-         * for which an authorization request have been performed.
-         * Given that no need to check if the authorization was previously authorized here is needed.
+         * been requested. The only status that has an exception from this is an
+         * expiration on ACTIVATED status that will make the transaction goes to the
+         * EXPIRED_NOT_AUTHORIZED final state. So is safe to assume that this applyEvent
+         * method will only be called for a transaction for which an authorization
+         * request have been performed. Given that no need to check if the authorization
+         * was previously authorized here is needed.
          */
-        return switch (event) {
-            case TransactionRefundErrorEvent e ->
-                    new TransactionWithRefundError((BaseTransactionWithRefundRequested) this.getTransactionAtPreviousState(), e);
-            case TransactionRefundRequestedEvent e ->
-                    new TransactionWithRefundRequested((BaseTransactionWithRequestedAuthorization) this.getTransactionAtPreviousState(), e);
-            default -> this;
-        };
-
+        if (event instanceof TransactionRefundRequestedEvent e) {
+            return new TransactionWithRefundRequested(
+                    (BaseTransactionWithRequestedAuthorization) this.getTransactionAtPreviousState(),
+                    e
+            );
+        }
+        return this;
     }
 
     /**
