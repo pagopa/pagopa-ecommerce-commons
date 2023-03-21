20e784b1b79c18f180d4ae969029ca0e78467f5a
Giovanni Berti <giovanni.berti@pagopa.it>
2023-03-21 10:27:51 +0100

    fix(docs): add missing docs for `PersonalDataVaultMetadata`

--- src/main/java/it/pagopa/ecommerce/commons/domain/PersonalDataVaultMetadata.java
+++ src/main/java/it/pagopa/ecommerce/commons/domain/PersonalDataVaultMetadata.java
@@ -5,6 +5,16 @@ import it.pagopa.ecommerce.commons.utils.ConfidentialDataManager;
 
 import javax.annotation.Nonnull;
 
+/**
+ * <p>
+ * Metadata class for confidential data encrypted through the Personal Data
+ * Vault service.
+ * </p>
+ * <p>
+ * This record is empty on purpose, as no additional data is needed to decrypt
+ * secrets (other than the received token).
+ * </p>
+ */
 @JsonIgnoreProperties(value = "mode")
 public record PersonalDataVaultMetadata()
         implements
