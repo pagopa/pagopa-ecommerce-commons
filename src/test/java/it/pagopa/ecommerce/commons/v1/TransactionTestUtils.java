9364bc5d042fbdd4c32ec3e491e7428d3fbc10d3
Giovanni Berti <giovanni.berti@pagopa.it>
2023-03-22 11:24:27 +0100

    refactor(test): remove pre-constructed test `ConfidentialDataManager`

--- src/test/java/it/pagopa/ecommerce/commons/v1/TransactionTestUtils.java
+++ src/test/java/it/pagopa/ecommerce/commons/v1/TransactionTestUtils.java
@@ -3,12 +3,11 @@ package it.pagopa.ecommerce.commons.v1;
 import it.pagopa.ecommerce.commons.documents.v1.Transaction;
 import it.pagopa.ecommerce.commons.documents.v1.*;
 import it.pagopa.ecommerce.commons.domain.Confidential;
+import it.pagopa.ecommerce.commons.domain.PersonalDataVaultMetadata;
 import it.pagopa.ecommerce.commons.domain.v1.*;
 import it.pagopa.ecommerce.commons.domain.v1.pojos.*;
 import it.pagopa.ecommerce.commons.generated.server.model.AuthorizationResultDto;
 import it.pagopa.ecommerce.commons.generated.server.model.TransactionStatusDto;
-import it.pagopa.ecommerce.commons.utils.ConfidentialDataManager;
-import it.pagopa.generated.pdv.v1.api.TokenApi;
 
 import javax.annotation.Nonnull;
 import javax.crypto.spec.SecretKeySpec;
@@ -21,19 +20,17 @@ public class TransactionTestUtils {
         // helper class with only static methods, no need to instantiate it
     }
 
-    public static final ConfidentialDataManager confidentialDataManager = constructConfidentialDataManager();
-
     public static final String RPT_ID = "77777777777111111111111111111";
     public static final String PAYMENT_TOKEN = "paymentToken";
     public static final String DESCRIPTION = "description";
     public static final int AMOUNT = 100;
-    public static final Confidential<Email> EMAIL;
 
-    static {
-        EMAIL = confidentialDataManager
-                .encrypt(ConfidentialDataManager.Mode.AES_GCM_NOPAD, new Email("foo@example.com"))
-                .block();
-    }
+    public static final String EMAIL_STRING = "foo@example.com";
+
+    public static final Confidential<Email> EMAIL = new Confidential<>(
+            new PersonalDataVaultMetadata(),
+            UUID.randomUUID().toString()
+    );
 
     public static final String FAULT_CODE = "";
     public static final String FAULT_CODE_STRING = "";
@@ -52,11 +49,12 @@ public class TransactionTestUtils {
     public static final String AUTHORIZATION_REQUEST_ID = UUID.randomUUID().toString();
     public static final String TRANSACTION_ID = UUID.randomUUID().toString();
 
-    private static ConfidentialDataManager constructConfidentialDataManager() {
+    @Nonnull
+    public static SecretKeySpec constructKeySpec() {
         byte[] key = new byte[16];
         new Random().nextBytes(key);
 
-        return new ConfidentialDataManager(new SecretKeySpec(key, "AES"), new TokenApi());
+        return new SecretKeySpec(key, "AES");
     }
 
     @Nonnull
