b29fe623df38aada24116ca68be12d8434cd6c2e
Giovanni Berti <giovanni.berti@pagopa.it>
2023-07-11 09:49:12 +0200

    feat(tracing): add `TracingUtils` methods for consumers

--- src/main/java/it/pagopa/ecommerce/commons/queues/TracingUtils.java
+++ src/main/java/it/pagopa/ecommerce/commons/queues/TracingUtils.java
@@ -5,14 +5,20 @@ import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.context.Context;
+import io.opentelemetry.context.propagation.TextMapGetter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.lang.NonNull;
 import reactor.core.publisher.Mono;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.HashMap;
 import java.util.Optional;
+import java.util.Set;
 import java.util.function.Function;
+
+import static com.azure.core.util.tracing.Tracer.PARENT_TRACE_CONTEXT_KEY;
 
 /**
  * <p>
@@ -21,6 +27,21 @@ import java.util.function.Function;
  * </p>
  */
 public class TracingUtils {
+    /**
+     * Constant for traceparent header name
+     */
+    public static final String TRACEPARENT = "traceparent";
+
+    /**
+     * Constant for tracestate header name
+     */
+    public static final String TRACESTATE = "tracestate";
+
+    /**
+     * Constant for baggage header name
+     */
+    public static final String BAGGAGE = "baggage";
+
     private static final Logger logger = LoggerFactory.getLogger(TracingUtils.class);
 
     private final OpenTelemetry openTelemetry;
@@ -54,8 +75,8 @@ public class TracingUtils {
 
     /**
      * <p>
-     * Wraps a {@link Mono} with a {@link Span} with name {@code spanName}. The new
-     * span is created as a child of the current span.
+     * Wraps a {@link Mono} with a new {@link Span} with name {@code spanName}. The
+     * new span is created as a child of the current span.
      * </p>
      * <p>
      * This method allows creation of
@@ -94,15 +115,94 @@ public class TracingUtils {
                     logger.debug("Raw tracing info: {}", rawTracingInfo);
 
                     TracingInfo tracingInfo = new TracingInfo(
-                            rawTracingInfo.get("traceparent"),
-                            Optional.ofNullable(rawTracingInfo.get("tracestate")),
-                            Optional.ofNullable(rawTracingInfo.get("baggage"))
+                            rawTracingInfo.get(TRACEPARENT),
+                            Optional.ofNullable(rawTracingInfo.get(TRACESTATE)),
+                            Optional.ofNullable(rawTracingInfo.get(BAGGAGE))
                     );
 
                     return new TracingContext(span, tracingInfo);
                 },
                 tracingContext -> traced.apply(tracingContext.tracingInfo),
                 tracingContext -> tracingContext.span.end()
+        );
+    }
+
+    /**
+     * <p>
+     * Wraps a {@link Mono} with a new {@link Span} with name {@code spanName}. The
+     * new span is created with a link to a remote span whose tracing data is
+     * extracted from {@code tracingInfo}.
+     * </p>
+     *
+     * @param tracingInfo tracing info to reconstruct the remote span to be linked
+     *                    to
+     * @param spanName    name of the new span
+     * @param operation   {@link Mono} that will be wrapped
+     * @return a new {@link Mono} with a span linked to the remote span identified
+     *         by {@code tracingInfo}
+     * @param <T> type parameter for the original {@link Mono}
+     */
+    public <T> @NonNull Mono<T> traceMonoWithRemoteSpan(
+                                                        @NonNull TracingInfo tracingInfo,
+                                                        @NonNull String spanName,
+                                                        @NonNull Mono<T> operation
+    ) {
+        Span span = createSpanWithRemoteTracingContext(tracingInfo, spanName);
+
+        return traceMonoWithSpan(span, operation);
+    }
+
+    private @NonNull Span createSpanWithRemoteTracingContext(
+                                                             @NonNull TracingInfo tracingInfo,
+                                                             @NonNull String spanName
+    ) {
+        logger.debug("Creating Span with remote tracing context: {}", tracingInfo);
+
+        Context extractedContext = openTelemetry.getPropagators().getTextMapPropagator().extract(
+                Context.current(),
+                tracingInfo,
+                new TextMapGetter<>() {
+                    @Override
+                    public Iterable<String> keys(@Nonnull TracingInfo tracingInfo) {
+                        return Set.of(TRACEPARENT, TRACESTATE, BAGGAGE);
+                    }
+
+                    @Nullable
+                    @Override
+                    public String get(
+                                      @Nullable TracingInfo tracingInfo,
+                                      @Nonnull String key
+                    ) {
+                        return switch (key) {
+                            case TRACEPARENT -> Optional.ofNullable(tracingInfo).map(TracingInfo::getTraceparent)
+                                    .orElse(null);
+                            case TRACESTATE -> Optional.ofNullable(tracingInfo).flatMap(TracingInfo::getTracestate)
+                                    .orElse(null);
+                            case BAGGAGE -> Optional.ofNullable(tracingInfo).flatMap(TracingInfo::getBaggage)
+                                    .orElse(null);
+                            default -> null;
+                        };
+                    }
+                }
+        );
+
+        return tracer
+                .spanBuilder(spanName)
+                .setSpanKind(SpanKind.CONSUMER)
+                .addLink(Span.fromContext(extractedContext).getSpanContext())
+                .startSpan();
+    }
+
+    private <T> @NonNull Mono<T> traceMonoWithSpan(
+                                                   @NonNull Span span,
+                                                   @NonNull Mono<T> operation
+    ) {
+        return Mono.using(
+                () -> span,
+                s -> operation.contextWrite(
+                        reactor.util.context.Context.of(PARENT_TRACE_CONTEXT_KEY, Context.current().with(s))
+                ),
+                Span::end
         );
     }
 }
