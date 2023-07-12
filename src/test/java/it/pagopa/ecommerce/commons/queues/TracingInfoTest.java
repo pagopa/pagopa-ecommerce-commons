93db997d50ccb815da97d33edf44efcb216cfc43
Giovanni Berti <giovanni.berti@pagopa.it>
2023-07-12 16:09:15 +0200

    test(tracing): add tests for `TracingInfo`

--- src/test/java/it/pagopa/ecommerce/commons/queues/TracingInfoTest.java
+++ src/test/java/it/pagopa/ecommerce/commons/queues/TracingInfoTest.java
@@ -1,11 +1,25 @@
 package it.pagopa.ecommerce.commons.queues;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(MockitoExtension.class)
 public class TracingInfoTest {
     public static final TracingInfo MOCK_TRACING_INFO = new TracingInfo(
             "mock_traceparent",
             Optional.of("mock_tracestate"),
             Optional.of("mock_baggage")
     );
+
+    @Test
+    public void throwsOnNullValues() {
+        assertThrows(Exception.class, () -> new TracingInfo(null, Optional.of(""), Optional.of("")));
+        assertThrows(Exception.class, () -> new TracingInfo("", null, Optional.of("")));
+        assertThrows(Exception.class, () -> new TracingInfo("", Optional.of(""), null));
+    }
 }
