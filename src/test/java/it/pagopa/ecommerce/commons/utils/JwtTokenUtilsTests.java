9dc0cab030e1a4c0807ce54276a594ffebe17666
Fernando-Granato <fernando.granato@pagopa.it>
2023-12-12 15:23:45 +0100

    fix: added tests coverage

--- src/test/java/it/pagopa/ecommerce/commons/utils/JwtTokenUtilsTests.java
+++ src/test/java/it/pagopa/ecommerce/commons/utils/JwtTokenUtilsTests.java
@@ -1,24 +1,32 @@
 package it.pagopa.ecommerce.commons.utils;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import it.pagopa.ecommerce.commons.domain.TransactionId;
+import it.pagopa.ecommerce.commons.exceptions.JWTTokenGenerationException;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
 import javax.crypto.SecretKey;
 import java.time.Duration;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doThrow;
 
 class JwtTokenUtilsTests {
     private static final String STRONG_KEY = "ODMzNUZBNTZENDg3NTYyREUyNDhGNDdCRUZDNzI3NDMzMzQwNTFEREZGQ0MyQzA5Mjc1RjY2NTQ1NDk5MDMxNzU5NDc0NUVFMTdDMDhGNzk4Q0Q3RENFMEJBODE1NURDREExNEY2Mzk4QzFEMTU0NTExNjUyMEExMzMwMTdDMDk";
-
     private static final int TOKEN_VALIDITY_TIME_SECONDS = 900;
     private final SecretKey jwtSecretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(STRONG_KEY));
     private final JwtTokenUtils jwtTokenUtils = new JwtTokenUtils();
+
 
     @Test
     void shouldGenerateValidJwtTokenWithOrderIdAndTransactionId() {
@@ -99,5 +107,31 @@ class JwtTokenUtilsTests {
                 Duration.ofSeconds(TOKEN_VALIDITY_TIME_SECONDS).toMillis(),
                 claims.getExpiration().getTime() - claims.getIssuedAt().getTime()
         );
+    }
+
+    @Test
+    void shouldGenerateExceptionJWTTokenGenerationException() {
+        TransactionId transactionId = new TransactionId(UUID.randomUUID());
+        String orderId = UUID.randomUUID().toString();
+        String paymentMethodId = UUID.randomUUID().toString();
+        it.pagopa.ecommerce.commons.domain.Claims jwtClaims = new it.pagopa.ecommerce.commons.domain.Claims(
+                transactionId,
+                orderId,
+                paymentMethodId
+        );
+        try (MockedStatic<Jwts> mockedJwts = Mockito.mockStatic(Jwts.class)) {
+            JwtBuilder jwtBuilder = Mockito.mock(JwtBuilder.class);
+            mockedJwts.when(Jwts::builder).thenReturn(jwtBuilder);
+            given(jwtBuilder.setId(any())).willReturn(jwtBuilder);
+            given(jwtBuilder.setIssuedAt(any())).willReturn(jwtBuilder);
+            given(jwtBuilder.setExpiration(any())).willReturn(jwtBuilder);
+            given(jwtBuilder.signWith(any())).willReturn(jwtBuilder);
+            given(jwtBuilder.claim(any(),any())).willReturn(jwtBuilder);
+            doThrow(new JwtException("Exception")).when(jwtBuilder).compact();
+
+            assertThrows(JWTTokenGenerationException.class, () -> jwtTokenUtils
+                .generateToken(jwtSecretKey, TOKEN_VALIDITY_TIME_SECONDS, jwtClaims)
+                .block());
+        }
     }
 }
