9f62cae520d20057b3058da5aa3681abf458653e
Pietro Tota <115724836+pietro-tota@users.noreply.github.com>
2024-03-05 14:01:37 +0100

    chore(refactoring): refactoring from checkout redirect to redirect + remove unused logic (#183)
    * chore(refactoring): refactoring from checkout redirect to redirect + remove unused logic
    
    * chore(refactoring): update javadoc

--- src/main/java/it/pagopa/ecommerce/commons/exceptions/RedirectMissingPspRequestedException.java
+++ src/main/java/it/pagopa/ecommerce/commons/exceptions/RedirectMissingPspRequestedException.java
@@ -3,10 +3,10 @@ package it.pagopa.ecommerce.commons.exceptions;
 import java.util.Set;
 
 /**
- * Exception thrown when requesting an API key from NPG configuration for a
- * nonexisting PSP
+ * Exception thrown when requesting a missing configuration for Redirect payment
+ * flow for a certain psp id
  */
-public class CheckoutRedirectMissingPspRequestedException extends RuntimeException {
+public class RedirectMissingPspRequestedException extends RuntimeException {
 
     /**
      * Constructor
@@ -15,10 +15,10 @@ public class CheckoutRedirectMissingPspRequestedException extends RuntimeExcepti
      * @param availablePsps     currently configured PSPs
      * @param configurationType configuration type
      */
-    public CheckoutRedirectMissingPspRequestedException(
+    public RedirectMissingPspRequestedException(
             String psp,
             Set<String> availablePsps,
-            CheckoutRedirectConfigurationType configurationType
+            RedirectConfigurationType configurationType
     ) {
         super(
                 "Requested configuration value in %s not available for PSP %s. Available PSPs: %s"
