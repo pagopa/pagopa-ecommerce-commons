b4a9fe4aa1e618f5eb58b44fc3fe35c0f1f17339
ciuffagianluca <113357981+ciuffagianluca@users.noreply.github.com>
2024-06-04 09:41:25 +0200

    [CHK2861] fix(jwt-token): add userId in possible claims of jwt token (#206)
    * add userId in possible claims of jwt token
    
    * use UUID in place of String to avoid null pointer in Claims constructor
    
    ---------
    
    Co-authored-by: Gianluca Ciuffa <gianlucaciuffa@MBP-di-Gianluca.homenet.telecomitalia.it>

--- src/main/java/it/pagopa/ecommerce/commons/domain/Claims.java
+++ src/main/java/it/pagopa/ecommerce/commons/domain/Claims.java
@@ -3,6 +3,7 @@ package it.pagopa.ecommerce.commons.domain;
 import it.pagopa.ecommerce.commons.annotations.ValueObject;
 
 import javax.annotation.Nullable;
+import java.util.UUID;
 
 /**
  * <p>
@@ -12,11 +13,14 @@ import javax.annotation.Nullable;
  * @param transactionId   transactionId
  * @param orderId         orderId
  * @param paymentMethodId payment methodId
+ * @param userId          the userId identified by the wallet token that
+ *                        performs the payment
  */
 @ValueObject
 public record Claims(
         @Nullable TransactionId transactionId,
         @Nullable String orderId,
-        @Nullable String paymentMethodId
+        @Nullable String paymentMethodId,
+        @Nullable UUID userId
 ) {
 }
