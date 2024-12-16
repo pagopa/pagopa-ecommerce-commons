6188898d67e5ea2c8709917578ac8a3394750d77
Simone infante <52280205+infantesimone@users.noreply.github.com>
2024-12-16 15:14:45 +0100

    fix(getPaymentEndToEndId): PPABV-95  fix paymentMethod for paymentEndToEndId (#226)
    * fix(getPaymentEndToEndId): fix paymentMethod
    
    * fix(getPaymentEndToEndId): handle method
    
    * test: add unit test for invalid paymentMethod

--- src/main/java/it/pagopa/ecommerce/commons/utils/NpgClientUtils.java
+++ src/main/java/it/pagopa/ecommerce/commons/utils/NpgClientUtils.java
@@ -90,16 +90,24 @@ public class NpgClientUtils {
                 .ofNullable(operationDto)
                 .filter(op -> op.getPaymentCircuit() != null)
                 .flatMap(
-                        op -> Optional.ofNullable(op.getAdditionalData())
-                )
-                .map(
-                        additionalData -> switch (NpgClient.PaymentMethod
-                                .fromServiceName(operationDto.getPaymentCircuit())) {
-                        case BANCOMATPAY -> additionalData.get(EndToEndId.BANCOMAT_PAY.getValue()).toString();
-                        case MYBANK -> additionalData.get(EndToEndId.MYBANK.getValue()).toString();
-                        default -> paymentEndToEndId;
-                        }
+                        op -> getValidPaymentMethod(op.getPaymentCircuit()).flatMap(
+                                paymentMethod -> Optional.ofNullable(op.getAdditionalData())
+                                        .map(additionalData -> switch (paymentMethod) {
+                                        case BANCOMATPAY -> additionalData.get(EndToEndId.BANCOMAT_PAY.getValue())
+                                                .toString();
+                                        case MYBANK -> additionalData.get(EndToEndId.MYBANK.getValue()).toString();
+                                        default -> paymentEndToEndId;
+                                        })
+                        )
                 )
                 .orElse(paymentEndToEndId);
+    }
+
+    private static Optional<NpgClient.PaymentMethod> getValidPaymentMethod(String paymentCircuit) {
+        try {
+            return Optional.of(NpgClient.PaymentMethod.fromServiceName(paymentCircuit));
+        } catch (IllegalArgumentException e) {
+            return Optional.empty();
+        }
     }
 }
