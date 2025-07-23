975258d173ebf95d813c6f9ed4d51bf3d2ea6485
danilo.ciano <danilo.ciano@pagopa.it>
2025-07-23 15:45:05 +0200

    feat(amount): update amount to long type from int

--- src/test/java/it/pagopa/ecommerce/commons/utils/v2/EuroUtilsTest.java
+++ src/test/java/it/pagopa/ecommerce/commons/utils/v2/EuroUtilsTest.java
@@ -13,8 +13,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class EuroUtilsTest {
 
     @Test
-    void shouldConvertEuroCentToEuroSuccessfully() {
+    void shouldConvertLongEuroCentToEuroSuccessfully() {
         BigDecimal euro = EuroUtils.euroCentsToEuro(111L);
+        System.out.println(euro);
+        assertEquals("1.11", euro.toString());
+    }
+
+    @Test
+    void shouldConvertIntEuroCentToEuroSuccessfully() {
+        BigDecimal euro = EuroUtils.euroCentsToEuro(111);
         System.out.println(euro);
         assertEquals("1.11", euro.toString());
     }
