b5b118471867d995db4bf0f197d077a65ed7f89d
Pietro Tota <pietro.tota@pagopa.it>
2025-09-05 14:28:37 +0200

    chore: fix trimming filter check

--- src/main/java/it/pagopa/ecommerce/commons/redis/reactivetemplatewrappers/ReactiveRedisTemplateWrapper.java
+++ src/main/java/it/pagopa/ecommerce/commons/redis/reactivetemplatewrappers/ReactiveRedisTemplateWrapper.java
@@ -144,7 +144,6 @@ public abstract class ReactiveRedisTemplateWrapper<V> {
      * @param key - the entity key for which retrieve TTL
      * @return a {@link Mono} emitting the TTL {@link Duration}; may be empty if no
      *         expiration is set
-     *
      * @see org.springframework.data.redis.core.ReactiveRedisOperations#getExpire(Object)
      */
     public Mono<Duration> getTTL(String key) {
@@ -184,7 +183,7 @@ public abstract class ReactiveRedisTemplateWrapper<V> {
     ) {
         return Mono
                 .just(streamSize)
-                .filter(size -> size > 0)
+                .filter(size -> size >= 0)
                 .switchIfEmpty(
                         Mono.error(
                                 new IllegalArgumentException(
